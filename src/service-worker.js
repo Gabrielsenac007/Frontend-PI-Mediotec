7711fd8fda16a6d2909db08516d87b8ec44c97dd
Gabriel <gabrielvdcsilva@gmail.com>
2024-12-02 17:37:11 -0300

    update pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,22 +1,47 @@
+const CACHE_NAME = 'app-cache-v1';
+
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/favicon-li9hXLfP.ico',
+  '/assets/index-BAp5TO6g.css',
+  '/assets/index-C52_a5r4.js',
+];
+
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('app-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/static/js/bundle.js',
-          '/static/css/main.css'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log('[Service Worker] Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Cacheando arquivos iniciais...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Ativando...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('[Service Worker] Removendo cache antigo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
